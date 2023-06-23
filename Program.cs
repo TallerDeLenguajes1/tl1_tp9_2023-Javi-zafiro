44832a269200fb65db5eb3acc65c9f56485a2e06
Javi-zafiro <javialexiscortez2@gmail.com>
2023-06-23 16:14:04 -0300

    primer comoit punto 1 listo

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Clase;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+
+var url = $"https://api.coindesk.com/v1/bpi/currentprice.json";
+var request = (HttpWebRequest)WebRequest.Create(url);
+request.Method = "GET";
+request.ContentType = "application/json";
+request.Accept = "application/json";
+try
+{
+    using (WebResponse response = request.GetResponse())
+    {
+        using (Stream strReader = response.GetResponseStream())
+        {
+            if (strReader == null) return;
+            using (StreamReader objReader = new StreamReader(strReader))
+            {
+                string responseBody = objReader.ReadToEnd();
+                Root Precios = JsonSerializer.Deserialize<Root>(responseBody);
+                Console.WriteLine(Precios.bpi.EUR.rate);
+                Console.WriteLine(Precios.bpi.USD.rate);
+                Console.WriteLine(Precios.bpi.GBP.rate);
+
+                Console.WriteLine(Precios.bpi.EUR.code);
+                Console.WriteLine(Precios.bpi.EUR.description);
+                Console.WriteLine(Precios.bpi.EUR.rate);
+                Console.WriteLine(Precios.bpi.EUR.rate_float);
+                Console.WriteLine(Precios.bpi.EUR.symbol);
+            }
+        }
+    }
+}
+catch (WebException ex)
+{
+    Console.WriteLine("Problemas de acceso a la API");
+}
